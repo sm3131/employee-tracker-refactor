a62517591b97c8a69c2a1db3b7f36b961bfd7586
sm3131 <sm2683@nau.edu>
2022-01-30 14:27:06 -0700

    changed some seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,11 +21,10 @@ VALUES
 
 INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES
-('Walter', 'White', 4, 3),
-('Jesse', 'Pinkman', 3, 1),
+('Walter', 'White', 4, 2),
 ('Gus', 'Fring', 1, null),
-('Hank', 'Schrader', 7, null),
-('Huell', 'Babineaux', 8, 8),
+('Hank', 'Schrader', 7, 6),
+('Huell', 'Babineaux', 8, 7),
 ('Skyler', 'White', 5, 1),
-('Mike', 'Ehrmantraut', 2, 3),
-('Saul', 'Goodman', 6, null);
+('Mike', 'Ehrmantraut', 2, 2),
+('Saul', 'Goodman', 6, 2);
